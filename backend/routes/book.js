75b5521e29c7df865583ddb3d0a1da0434523ef3
NopXx <97354576+NopXx@users.noreply.github.com>
2023-02-14 14:20:15 +0700

    update: add path api

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,13 +4,44 @@ const router = express.Router()
 const db = require('../lib/db.js')
 
 router.get('/books', (req, res) => {
-    db.query('select * from tb_book', (err, data) => {
-        if (err) {
-            return res.status(401).send(err)
-        } else {
-            return res.status(200).send(data)
-        }
-    })
+  const search = req.query.search
+  db.query(
+    `select * from tb_book`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send(err)
+      } else {
+        return res.status(200).send(data)
+      }
+    }
+  )
+})
+router.get('/books/search', (req, res) => {
+  const search = req.query.value
+  db.query(
+    `select * from tb_book where b_id like '%${search}%' or b_name like '%${search}%' or b_writer like '%${search}%' or b_price like '%${search}%'`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send(err)
+      } else {
+        return res.status(200).send(data)
+      }
+    }
+  )
+})
+router.get('/book', (req, res) => {
+  const table = req.query.table
+  const value = req.query.value
+  db.query(
+    `select * from tb_book where ${table} like '%${value}%'`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send(err)
+      } else {
+        return res.status(200).send(data)
+      }
+    }
+  )
 })
 
 module.exports = router
